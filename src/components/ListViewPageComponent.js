5a206c3380e6302cd7b46f4b392b999e3029a91b
Roope Uitto <roouit@gmail.com>
2021-12-07 14:57:47 +0200

    New: can sort and filter with multiple lists

--- src/components/ListViewPageComponent.js
+++ src/components/ListViewPageComponent.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { setListsToShow } from '../redux'
+import { setListsToShow, updateTodo } from '../redux'
+import moment from 'moment'
 import './ListViewPageComponent.css'
 import ListViewComponent from './ListViewComponent'
 
 function ListViewPageComponent () {
-  // eslint-disable-next-line no-unused-vars
   const [filters, setFilters] = useState([])
+  const [sortByLastModified, setSortByLastModified] = useState(false)
+  const [update, setUpdate] = useState(false)
   const [listCount, setListCount] = useState(3)
   const listData = useSelector(state => state.todo)
   const lists = useSelector((state) => state.list)
@@ -25,6 +27,14 @@ function ListViewPageComponent () {
   }, [])
 
   useEffect(() => {
+    if (sortByLastModified && update) {
+      const newListData = getSortedTodoList()
+      setUpdate(false)
+      dispatch(updateTodo(newListData))
+    }
+  }, [listData])
+
+  useEffect(() => {
     const newLists = [...lists.listsToShow]
     if (newLists.length > listCount) {
       dispatch(setListsToShow(newLists.slice(0, listCount)))
@@ -40,6 +50,47 @@ function ListViewPageComponent () {
       dispatch(setListsToShow(newLists))
     }
   }, [listCount])
+
+  const getSortedTodoList = () => {
+    const newListData = {
+      ...listData
+    }
+    lists.listsToShow.forEach(list => {
+      if (list !== 'empty') {
+        const sortedArray = listData.lists[list].todoIds.sort(
+          (a, b) =>
+            moment(listData.todos[b].last_modified).valueOf() -
+            moment(listData.todos[a].last_modified).valueOf()
+        )
+        newListData.lists[list].todoIds = sortedArray
+      }
+    })
+    return newListData
+  }
+
+  const handleSortTodos = () => {
+    if (sortByLastModified === false) {
+      const newListData = getSortedTodoList()
+      dispatch(updateTodo(newListData))
+      setUpdate(true)
+    }
+    setSortByLastModified(!sortByLastModified)
+  }
+
+  const handleAddFilters = (e) => {
+    e.preventDefault()
+    if (e.target.todoFilters.value) {
+      setFilters([...filters, e.target.todoFilters.value])
+      e.target.todoFilters.value = ''
+    }
+  }
+
+  const handleRemoveFilter = (e) => {
+    const newFilters = [...filters].filter((filter) => {
+      return filter !== e.target.textContent
+    })
+    setFilters(newFilters)
+  }
 
   return (
     <div className='list-view-wrapper'>
@@ -63,6 +114,38 @@ function ListViewPageComponent () {
             alt='kolme palstaa'
             onClick={() => setListCount(3)}
           ></img>
+        </div>
+        <div className='sort-options'>
+          <label>
+            <input
+              className='todo-sort-by-edit'
+              type='checkbox'
+              defaultChecked={sortByLastModified}
+              onChange={() => handleSortTodos()}
+            ></input>
+            Järjestä automaattisesti muokkausajan mukaan
+          </label>
+          <form
+            className='filter-todos-form'
+            onSubmit={(e) => handleAddFilters(e)}
+          >
+            <input
+              type='text'
+              name='todoFilters'
+              className='filter-todos'
+              placeholder='Suodata tehtäviä..'
+            ></input>
+          </form>
+          <div>
+            {filters.map((filter) => {
+              return (
+                <span key={filter} onClick={(e) => handleRemoveFilter(e)}>
+                  {filter}
+                  <br></br>
+                </span>
+              )
+            })}
+          </div>
         </div>
       </div>
       <div className='list-view-lists'>
